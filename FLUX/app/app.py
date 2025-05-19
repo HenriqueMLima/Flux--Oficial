8aa76458feff0e9451b1295a2731474343637bba
henriqueMlima <henriquegamadf21@gmail.com>
2025-05-19 15:59:16 -0300

    conexão com banco

--- FLUX/app/app.py
+++ FLUX/app/app.py
@@ -1,2 +1,36 @@
-import flask
-import _mysql_connector
+from flask import Flask, request, jsonify
+import mysql.connector
+from mysql.connector import Error
+import bcrypt
+
+app = Flask(__name__)
+
+DB_CONFIG = {
+    'host': 'yamanote.proxy.rlwy.net',
+    'user': 'root',
+    'password': 'QUSURPzzbYPxKkFYsRKRhhWNXOdEiiMn',
+    'database': 'railway' ,
+    'port': '49036'
+}
+
+def get_db_connection():
+    try: 
+        connection = mysql.connector.connect(**DB_CONFIG)
+        if connection.is_connected():
+            print("conexão com o banco de dados MySQL estabelecida com sucesso")
+            return connection
+    except Error as e:
+        print(f"Erro ao conectar ao banco de dados MySQL {e}")
+    return None
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
