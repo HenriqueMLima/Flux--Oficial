6407b67312b59a8cb4a39dee508631c0b5223c37
henriqueMlima <henriquegamadf21@gmail.com>
2025-05-27 17:14:53 -0300

    ajustes no JS do processos /modelo

--- FLUX/app/app.py
+++ FLUX/app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import mysql.connector
 from mysql.connector import Error
 import bcrypt
 
 app = Flask(__name__)
+CORS(app)
 
 DB_CONFIG = {
     'host': 'yamanote.proxy.rlwy.net',
